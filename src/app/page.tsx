d6785962024406a45c8babe2b4e34d9aad5d9f86
niftables-ader <ader.oulmadou@niftables.com>
2024-09-30 15:03:25 +0100

    chore: change color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className="text-4xl text-[#cc5757] bg-[#191919]">Comming Soon...</div>
+    <div className="text-4xl text-[#c46565] bg-[#191919]">Comming Soon...</div>
   )
 }
