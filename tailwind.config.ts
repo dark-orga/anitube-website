d96a218aebbd0143587477cd94383ba203817726
Said Mazouz <smazouz@student.1337.ma>
2024-10-02 11:23:26 +0100

    feat: build a landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,33 @@
-import fluid, { extract, fontSize, screens } from 'fluid-tailwind'
+import { nextui } from '@nextui-org/theme'
+import fluid, { extract, fontSize,screens } from 'fluid-tailwind'
+import type { Config } from 'tailwindcss'
 
-const config = {
+const config: Config = {
   content: {
     files: [
+      './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
       './src/components/**/*.{js,ts,jsx,tsx,mdx}',
       './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+      './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
     ],
     extract,
   },
   theme: {
     screens,
     fontSize,
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+    },
   },
-  plugins: [fluid],
+  plugins: [
+    fluid({
+      checkSC144: false,
+    }),
+    nextui({}),
+  ],
 }
+
 export default config
