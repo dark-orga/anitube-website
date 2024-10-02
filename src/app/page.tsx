ce071aaad9778b0a9e2e7cfdc52eb89baf77de03
Said Mazouz <smazouz@student.1337.ma>
2024-10-02 11:23:26 +0100

    feat: build a landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import { LandingPage } from '@/components/LandingPage'
 
 export default function Home() {
-  return (
-    <div className="text-4xl text-[#c46565] bg-[#191919]">Comming Soon...</div>
-  )
+  return <LandingPage />
 }
