aeea2036a576e2e502e978a4f6b6fc93029bb692
Said Mazouz <smazouz@student.1337.ma>
2024-10-17 02:11:36 +0100

    chore: building swiper from scratch and adding a smooth animation when swiping slides

--- src/components/HomePageSlider/index.tsx
+++ src/components/HomePageSlider/index.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { ChevronLeft, ChevronRight } from 'lucide-react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+import { cn } from '@/lib/utils'
 
 import { Button } from '../ui/button'
 
@@ -44,19 +46,37 @@ export function ImageCarousel() {
 
   const handleNextSlide = () => {
     setIsTransitioning(true)
+    setCurrentIndex((currentIndex + 1) % slides.length)
+    setWidth(0)
+
     setTimeout(() => {
-      setCurrentIndex((currentIndex + 1) % slides.length)
       setIsTransitioning(false)
-    }, 200)
+    }, 400)
   }
 
   const handlePreviousSlide = () => {
     setIsTransitioning(true)
+    setWidth(0)
     setTimeout(() => {
       setCurrentIndex((currentIndex - 1 + slides.length) % slides.length)
       setIsTransitioning(false)
-    }, 200)
+    }, 400)
   }
+
+  const [width, setWidth] = useState(0)
+
+  useEffect(() => {
+    const time = setInterval(() => {
+      if (width < 100) setWidth(width + 1)
+      else {
+        handleNextSlide()
+      }
+    }, 50)
+
+    return () => {
+      clearInterval(time)
+    }
+  }, [width])
 
   return (
     <div className="h-[500px] w-full bg-black">
@@ -64,7 +84,7 @@ export function ImageCarousel() {
         <img
           src={slides[currentIndex].imgSrc}
           alt={`Slide ${currentIndex + 1}`}
-          className={`h-full w-full object-cover transition-opacity duration-500 ease-in-out ${isTransitioning ? 'opacity-0' : 'opacity-100'}`}
+          className={`h-full w-full object-cover transition-opacity duration-700 ease-in-out ${isTransitioning ? 'opacity-0' : 'opacity-100'}`}
         />
         <Button
           onClick={handlePreviousSlide}
@@ -78,12 +98,27 @@ export function ImageCarousel() {
         >
           <ChevronRight className="h-12 w-12 text-gray-100 drop-shadow-[0_0.3px_0.3px_rgba(0,0,0,1.1)]" />
         </Button>
-        <div className="absolute flex space-x-2">
+        <div className="absolute bottom-4 left-1/2 flex w-96 -translate-x-1/2 transform space-x-2">
           {slides.map((slide, index) => (
             <div
               key={index}
-              className={`h-2 rounded-full ${currentIndex === index ? 'w-16 bg-purple-600 dark:bg-purple-500' : 'w-8 bg-gray-600'}`}
-            ></div>
+              className={cn(
+                'h-2 w-8 transform overflow-hidden rounded-full bg-gray-600 duration-300',
+                { 'w-16': currentIndex === index },
+              )}
+            >
+              <div
+                className={cn(
+                  'h-full transform rounded-full bg-gray-600 duration-500 ease-linear',
+                  {
+                    'bg-purple-800': currentIndex === index,
+                  },
+                )}
+                style={{
+                  width: `${currentIndex === index ? width : 0}%`,
+                }}
+              ></div>
+            </div>
           ))}
         </div>
       </div>
